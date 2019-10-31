ccd8d4c08ff4db348cf099ebf21d97bc03ab7182
UoWHarvey <55839084+UoWHarvey@users.noreply.github.com>
2019-10-31 14:32:18 +0000

    saving

--- touchGestureCode(1)/touchGestureCode/www/js/index.js
+++ touchGestureCode(1)/touchGestureCode/www/js/index.js
@@ -12,12 +12,52 @@ $(document).on("pagecreate","#pageone",function(){
 
 	$('#swipetext').on("swipeleft",function(){
     	$(this).css('color', 'green');
-     ('#swipetext').on("swiperight",function(){
-        $(this).ss('color', 'blue');
-  	});
-        
-    $
     });
+
+    var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+function updateDisplay() {
+	$("#launched").text("Application launched: " + launched_count);
+	$("#resumed").text("Application paused: " + paused_count);
+	$("#paused").text("Application resumed: " + resumed_count);
+}
+
+
+// device APIs are available
+//
+    function onDeviceReady() {
+	
+	document.addEventListener("resume", onResume, false);
+	document.addEventListener("pause", onPause, false);
+	
+	launched_count++;
+	updateDisplay();
+	    
+	alert("device ready");
+    }
+
+
+    function onPause() {
+	
+	paused_count++;
+	updateDisplay();
+	    
+	alert("pause");
+    }
+	
+
+    function onResume() {
+		
+	resumed_count++;
+	updateDisplay();
+	    
+	alert("resume");
+    }
 
 
 });
